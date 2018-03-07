39483e5253159ad487edc24e1a34068e749d5541
CREATIVA\edgar.mejia <edgar13155@gmail.com>
2018-03-07 11:33:31 -0600

    Rutas funcionando.

--- src/app/layouts/home-layout/home-layout.component.ts
+++ src/app/layouts/home-layout/home-layout.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-home-layout',
-    template: `<app-sidebar></app-sidebar><router-outlet></router-outlet>`,
+    template: `<app-sidebar></app-sidebar>`,
     styles: []
 })
 export class HomeLayoutComponent implements OnInit {
