5463a005577f0f745cce45189a36f409477fc734
CREATIVA\edgar.mejia <edgar13155@gmail.com>
2018-03-08 16:14:59 -0600

    Nuevo diseño (rechazado).

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
@@ -6,4 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
+    @Input() sidenav;
 }
