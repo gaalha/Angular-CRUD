8b2f5185fd53f9c687965a524741bd1ff2e25273
Edgar Mejía <edgar13155@gmail.com>
2018-03-19 14:09:26 -0600

    Correcciones en el archivo index.pages

--- src/app/components/index.pages.ts
+++
@@ -1,9 +0,0 @@
-export { AboutComponent } from "./about/about.component";
-export { ContactUsComponent } from "./contact-us/contact-us.component";
-export { HomeComponent } from "./home/home.component";
-export { LoginComponent } from "../modules/login-layout/login/login.component";
-export { NotFoundComponent } from "./not-found/not-found.component";
-
-// CRUD
-export { PersonComponent } from "../modules/person/person.component";
-export { UserComponent } from "../modules/user/user.component";
---
+++ src/app/utils/index.pages.ts
@@ -0,0 +1,9 @@
+export { AboutComponent } from '../components/about/about.component';
+export { ContactUsComponent } from '../components/contact-us/contact-us.component';
+export { HomeComponent } from '../components/home/home.component';
+export { NotFoundComponent } from '../components/not-found/not-found.component';
+export { LoginComponent } from '../modules/login-layout/login/login.component';
+
+// CRUD
+export { PersonComponent } from "../modules/person/person.component";
+export { UserComponent } from "../modules/user/user.component";
