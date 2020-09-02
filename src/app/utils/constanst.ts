1c3800820e47a5efde81fa4a3161d44577feee2b
Edgar Mejia <edgar13155@gmail.com>
2020-09-01 23:41:49 -0600

    Update constants

--- src/app/utils/constanst.ts
+++ src/app/utils/constanst.ts
@@ -1,21 +1,26 @@
 const HOST = 'http://localhost:3000';
 
 export const CONSTANST = {
-    permissions: {},
-    routes: {
-        authorization: {
-            login: HOST + '/api/auth/login',
-            logout: HOST + '/api/auth/logout'
-        },
-        client: {
-            list: HOST + '/api/client',
-            delete: HOST + '/api/client/delete/:id',
-            save: HOST + '/api/client/save',
-            get: HOST + '/api/client/:id'
-        },
-        user: {}
+  permissions: {},
+  routes: {
+    authorization: {
+      login: HOST + '/api/auth/login',
+      logout: HOST + '/api/auth/logout'
     },
-    lang: {},
-    session: {},
-    parameters: {}
+    client: {
+      list: HOST + '/api/client',
+      delete: HOST + '/api/client/delete/:id',
+      save: HOST + '/api/client/save',
+      get: HOST + '/api/client/:id'
+    },
+    user: {
+      list: HOST + '/api/user',
+      delete: HOST + '/api/user/delete/:id',
+      save: HOST + '/api/user/save',
+      get: HOST + '/api/user/:id'
+    }
+  },
+  lang: {},
+  session: {},
+  parameters: {}
 };
